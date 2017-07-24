732f5f56b19820f5904e5773d3968d2369fdd849
Yeontae Kim <ytk007@gmail.com>
2017-07-24 14:02:17 -0700

    Add remove annotation and modify navigation bar item button

--- VirtualTourist/MapVC.swift
+++ VirtualTourist/MapVC.swift
@@ -12,23 +12,78 @@ import MapKit
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    let annotation = MKPointAnnotation()
+    var longPressGesture: UILongPressGestureRecognizer? = nil
+    var tapGesture: UITapGestureRecognizer? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotation))
-        longPressGesture.minimumPressDuration = 0.5
-        self.mapView.addGestureRecognizer(longPressGesture)
+        // Add Edit Button Programmatically
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editButtonPressed(_:)))
+        
+        // Set up gestures
+        longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotation))
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(removeAnnotation))
+        
+        // Add Long Press Gesture
+        addLongPressGesture()
+
     }
     
     func addAnnotation(gestureRecognizer:UIGestureRecognizer){
-        print("function invoked")
+        
         let touchPoint = gestureRecognizer.location(in: mapView)
         let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = newCoordinates
+        self.annotation.coordinate = newCoordinates
         mapView.addAnnotation(annotation)
     }
+    
+    func removeAnnotation(gesture: UIGestureRecognizer) {
+        
+        if gesture.state == UIGestureRecognizerState.ended {
+            self.mapView.removeAnnotation(annotation)
+        }
+        
+        resetBarButton(title: "Done")
+        
+    }
+    
+    func addTapGesture() {
+        self.tapGesture?.numberOfTapsRequired = 1
+        self.mapView.addGestureRecognizer(self.tapGesture!)
+    }
+    
+    func addLongPressGesture() {
+        self.longPressGesture?.minimumPressDuration = 0.5
+        self.mapView.addGestureRecognizer(self.longPressGesture!)
+    }
+    
+    func resetBarButton(title: String) {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: title, style: .plain, target: self, action: #selector(editButtonPressed(_:)))
+    }
 
+    @IBAction func editButtonPressed(_ sender: Any) {
+        
+        if self.navigationItem.rightBarButtonItem?.title == "Edit" {
+            
+            if let longPressGesture = longPressGesture {
+                view.removeGestureRecognizer(longPressGesture)
+            }
+            
+            addTapGesture()
+            resetBarButton(title: "Done")
+            
+        } else {
+            
+            if let tapGesture = tapGesture {
+                view.removeGestureRecognizer(tapGesture)
+            }
+            
+            addLongPressGesture()
+            resetBarButton(title: "Edit")
+        }
+        
+    }
 }
 
